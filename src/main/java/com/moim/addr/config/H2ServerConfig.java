7a02883731e184271513dd430493e5b3266ed029
cdssw <cdssw@naver.com>
2022-08-23 09:49:10 +0900

    java11로 변경, h2 외부접속 처리 추가

---
+++ src/main/java/com/moim/addr/config/H2ServerConfig.java
@@ -0,0 +1,26 @@
+package com.moim.addr.config;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.h2.tools.Server;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@Profile("test")
+public class H2ServerConfig {
+
+	@Bean
+	@ConfigurationProperties("spring.datasource.hikari")
+	public DataSource dataSource() throws SQLException {
+		Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "10016").start();
+		
+		return new HikariDataSource();
+	}
+}
